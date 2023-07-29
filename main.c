682b86eae07aea1a56fc828156dd6fdbe6a98631
Murilo Luis Calvo Neves <muriloluiscalvoneves2004@hotmail.com>
2023-07-29 00:05:26 -0300

    Criada estrutura de fluxo
    É só a estrutura que foi definida no diagrama do projeto, ainda não foi implementado nada

--- main.c
+++ main.c
@@ -1,38 +1,62 @@
-// Arquivo principal do trabalho
+/*
+    Trabalho 1 - Organização e recuperação de dados
+    Professora: Valeria Delisandra Feltrim
+    Alunos: 
+        Leonardo Venâncio Correia (RA: 129266)
+        Murilo Luis Calvo Neves (RA: 129037)
+*/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define TAM_MINIMO_SOBRA 10  // Tamanho mínimo que a sobra de um registro deve possuir para ser reaproveitada
+#define NOME_ARQUIVO_DADOS "dados.dat"
 
-typedef enum {False, True} booleano;
+// Se acabar não sendo usado, é bom remover
+typedef enum {False, True} booleano;  // Enum booleano para deixar o código mais limpo caso seja necessário
 
-void impressao_da_led(FILE* arquivo_de_dados);
-void fazer_operacoes(FILE* arquivo_de_dados, FILE* arquivo_de_operacoes);
+// Funções auxiliares
+// -- A criar
+
+// Funções de registros
+void inserir_registro(char* novo_registro);  // A implementar
+void remover_registro(char* identificador);  // A implementar
+void buscar_registro(char* identificador);  // A implementar
+
+// Funções de modos de operações
+void impressao_da_led(FILE* arquivo_de_dados);  // A implementar
+void fazer_operacoes(FILE* arquivo_de_dados, FILE* arquivo_de_operacoes);  // A implementar
 
 int main(int argc, char *argv[]) {
+    /*
+        Ponto de entrada principal do programa
+    */
 
+    FILE *arquivo_de_dados = fopen(NOME_ARQUIVO_DADOS, "rb+");
+
+    if (arquivo_de_dados == NULL)
+    {
+        fprintf(stderr, "\nNao foi encontrado o arquivo de dados para leitura");
+        exit(EXIT_FAILURE);
+    }
+    
     if (argc == 3 && strcmp(argv[1], "-e") == 0) {
 
         printf("Modo de execucao de operacoes ativado ... nome do arquivo = %s\n", argv[2]);
-        // chamada da funcao que executa o arquivo de operacoes
-        // o nome do arquivo de operacoes estara armazenado na variavel argv[2]
-        // executa_operacoes(argv[2])
+        FILE *arquivo_de_operacoes = fopen(argv[2], "rb");  // Como dito no enunciado, assume-se que esse arquivo sempre é aberto corretamente
+        fazer_operacoes(arquivo_de_dados, arquivo_de_operacoes);
 
     } else if (argc == 2 && strcmp(argv[1], "-p") == 0) {
 
         printf("Modo de impressao da LED ativado ...\n");
-        // chamada da funcao que imprime as informacoes da led
-        // imprime_led();
+        imprime_led(arquivo_de_dados);
 
     } else {
-
         fprintf(stderr, "Argumentos incorretos!\n");
         fprintf(stderr, "Modo de uso:\n");
         fprintf(stderr, "$ %s -e nome_arquivo\n", argv[0]);
         fprintf(stderr, "$ %s -p\n", argv[0]);
         exit(EXIT_FAILURE);
-
     }
 
     return 0;
@@ -53,7 +77,7 @@ int main(int argc, char *argv[]) {
     2- -- se a linha comeca com b, fazer_busca()
     3- -- se a linha comeca com i, fazer_insercao()
     4- -- se a linha comeca com r, fazer_remocao()
-    5- -- se a linha comeca com outra coisa (e nao for fim do arquivo), exibir erro
+    5- -- se a linha comeca com outra coisa (e nao for fim do arquivo), exibir erro [ou fazer nada]
 
     IMPRESSAO_DA_LED
     1- se o offset for '-1'
@@ -71,5 +95,3 @@ int main(int argc, char *argv[]) {
     FAZER_REMOCAO
     (A implementar) Remover registro e colocar seu offset na LED (que está decrescente) na posição correta
     */
-
-    // Pro windows não fechar o CMD imediatamente 
